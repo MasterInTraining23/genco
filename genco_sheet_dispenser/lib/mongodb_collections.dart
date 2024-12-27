d1d13dd509e1fc0054ef5c2d2c1d75bc7303bc28
adwemine <amadu23@gmail.com>
2024-12-27 00:54:24 -0500

    Refresh repo

--- genco_sheet_dispenser/lib/mongodb_collections.dart
+++
@@ -1,5 +0,0 @@
-String institutionCollectionName = "institution";
-String kioskFlowCollectionName = "kiosk_flow";
-String kioskPageInfoCollectionName = "kiosk_page_info";
-String usageMetricCollectionName = "usage_metric";
-String userCollectionName = "user";
