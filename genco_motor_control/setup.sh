60a0757b7e98783690d3d5c1bf3d04f1552b7f0c
MasterInTraining23 <amadu23@gmail.com>
2024-12-30 08:54:06 -0500

    Add files via upload

---
+++ genco_motor_control/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 -m venv /home/genco/workspace/genco/genco_motor_control/.venv
+source /home/genco/workspace/genco/genco_motor_control/.venv/bin/activate
+pip install -r /home/genco/workspace/genco/genco_motor_control/requirements.txt
