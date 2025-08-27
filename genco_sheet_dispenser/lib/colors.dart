c84d4663906b15303905592009e8a86ace50c813
Andrew Steinmetz <ando.steinmetz@proton.me>
2025-08-27 14:47:48 -0500

    Update home page with clear instructions.

---
+++ genco_sheet_dispenser/lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryRed = Colors.red;
+  static const Color errorText = Colors.black;
+  static const Color buttonText = Colors.red;
+  static const Color buttonBorder = Colors.red;
+  static const Color transparent = Colors.transparent;
+  static const Color lightRed = Color.fromARGB(255, 255, 200, 200);
+}
--- genco_sheet_dispenser/lib/home_page.dart
+++ genco_sheet_dispenser/lib/home_page.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'coordination_model.dart';
 import 'navigator.dart';
+import 'colors.dart';
+import 'models/institutions.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
+    String inputPrompt = (denverUniversityId == "1")  
+    ? "INPUT\nYOUR STUDENT ID" 
+    : "INPUT\nYOUR EMAIL";
+    Size screenSize = MediaQuery.of(context).size;
     final coordinationModel = Provider.of<CoordinationModel>(context);
 
     return Scaffold(
@@ -16,14 +21,63 @@ class HomePage extends StatelessWidget {
           onTap: () {
             navigateToNextCoordinatedPage(context, coordinationModel);
           },
-          child: Image.asset(
-            'assets/images/touch_me.gif',
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity,
+          child: Container(
+            width: screenSize.width,
+            height: screenSize.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  "FREE ZERO WASTE\nLAUNDRY DETERGENT!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 70,
+                    fontFamily: "Arial",
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Column(
+                    children: [
+                      Text(
+                        inputPrompt,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontFamily: "Arial",
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2.0,
+                        ),
+                      ),
+                      Container(
+                        height: 124,
+                        width: 629,
+                        decoration: BoxDecoration(
+                        border: Border.all(color: Colors.red, width: 2),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "TOUCH TO START",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                          fontFamily: 'Arial',
+                          fontSize: 42,
+                          color: AppColors.lightRed,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      ),
+                    ),
+                  ),
+                    ],
+                    )
+                  ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      )
+      );
   }
 }
